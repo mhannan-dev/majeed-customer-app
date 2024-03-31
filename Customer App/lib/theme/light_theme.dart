40b7dec4d948f7666e59565fee1d236d6d035405
Muhammad Hannan <mdhannan.info@gmail.com>
2024-03-31 16:57:50 +0600

    Update color

--- Customer App/lib/theme/light_theme.dart
+++ Customer App/lib/theme/light_theme.dart
@@ -3,19 +3,19 @@ import 'package:flutter/material.dart';
 
 ThemeData light = ThemeData(
   fontFamily: AppConstants.fontFamily,
-  primaryColor: const Color(0xFFFF8200),
+  primaryColor: const Color(0xFFE2B951),
   secondaryHeaderColor: const Color(0xFF1ED7AA),
   disabledColor: const Color(0xFFBABFC4),
   brightness: Brightness.light,
   hintColor: const Color(0xFF9F9F9F),
   cardColor: Colors.white,
   textButtonTheme: TextButtonThemeData(
-      style: TextButton.styleFrom(foregroundColor: const Color(0xFFFF8200))),
+      style: TextButton.styleFrom(foregroundColor: const Color(0xFFE2B951))),
   colorScheme: const ColorScheme.light(
-          primary: Color(0xFFFF8200),
+          primary: Color(0xFFE2B951),
           tertiary: Color(0xff6165D7),
           tertiaryContainer: Color(0xff171DB6),
-          secondary: Color(0xFFFF8200))
+          secondary: Color(0xFFE2B951))
       .copyWith(background: const Color(0xFFF3F3F3))
       .copyWith(error: const Color(0xFFE84D4F)),
   popupMenuTheme: const PopupMenuThemeData(
